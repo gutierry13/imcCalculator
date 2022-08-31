910aca21fd080c56e3dafd18454ad5d4696dca62
Alexandre Gutierry <aleguti1000@hotmail.com>
2022-08-31 15:12:35 -0300

    Projeto modularizado

---
+++ Js/modal.js
@@ -0,0 +1,16 @@
+export const Modal = {
+  modalButton: document.querySelector('.close'),
+  modal: document.querySelector('.modal-wrapper'),
+  imcResultH2: document.querySelector('.modal-wrapper span'),
+
+  open() {
+    this.modal.classList.add('open')
+  },
+  close() {
+    this.modal.classList.remove('open')
+  }
+}
+function HandleClose() {
+  Modal.close()
+}
+Modal.modalButton.addEventListener('click', HandleClose)
--- Js/script.js
+++ Js/script.js
@@ -1,20 +1,9 @@
+import { Modal } from './modal.js'
 const weight = document.querySelector('#weight')
 const height = document.querySelector('#height')
 const button = document.querySelector('form button')
 const alertError = document.querySelector('.alert-error')
 
-const Modal = {
-  modalButton: document.querySelector('.close'),
-  modal: document.querySelector('.modal-wrapper'),
-  imcResultH2: document.querySelector('.modal-wrapper span'),
-
-  open() {
-    this.modal.classList.add('open')
-  },
-  close() {
-    this.modal.classList.remove('open')
-  }
-}
 function handleCalculator(event) {
   event.preventDefault()
   const numberWeight = Number(weight.value)
@@ -31,7 +20,3 @@ function handleCalculator(event) {
   }
 }
 button.addEventListener('click', handleCalculator)
-function HandleClose() {
-  Modal.close()
-}
-Modal.modalButton.addEventListener('click', HandleClose)
